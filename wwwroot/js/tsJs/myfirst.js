30e0bea96461fd537750474e8ba71ccd5f01735f
Martin Korneffel <Martin.Korneffel@mk-prg-net.de>
2023-02-28 00:44:19 +0100

    nochmal

---
+++ wwwroot/js/tsJs/myfirst.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=myfirst.js.map
---
+++ wwwroot/typings/vue.d.ts
@@ -0,0 +1,9 @@
+import { CompilerOptions } from './VueCompilerDom';
+import { RenderFunction } from './VueRuntimeDom';
+
+export declare function compile(template: string | HTMLElement, options?: CompilerOptions): RenderFunction;
+
+
+export * from "./VueRuntimeDom";
+
+export { }
